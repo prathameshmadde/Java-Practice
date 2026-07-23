335a27d296c994a67a706e43488e458d7c0d6e15
Prathamesh Madde. <136072111+prathameshmadde@users.noreply.github.com>
2026-07-23 22:41:34 +0530

    new codes

---
+++ 22.07.26/OneEven.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class  OneEven
+{
+	public static void main(String[] args) 
+	{	Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number : ");
+		
+		int num =sc.nextInt();
+		int count=0;
+		while (num!=0)
+		{	
+			int last = num%10;
+			if(last %2 == 0)
+			{
+				count++;
+			}
+			num/=10;
+		}
+		
+		if(count==1)	{
+			System.out.println("It contain only one Even digit .");
+		}
+		else if(count==0){
+			System.out.println("It dosent cntain any even digit no .");
+			
+		}
+		else {
+			System.out.println("It contain more than one even digit no .");
+		}
+		
+	}
+}
